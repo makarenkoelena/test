3e6ed1055697150c09ba30e892bc084d234fa34c
ELENA MAKARENKO - STUDENT <G00341964@gmit.ie>
2019-02-13 19:39:40 +0000

    second project added

---
+++ FitnessApp/FitnessApp/FitnessApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FitnessApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            // this.CurrentPage = this.Children[0];
+        }
+
+        private void Register_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new RegisterPage());
+        }
+
+        private void Login_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new LoginPage());
+        }
+    }
+}
